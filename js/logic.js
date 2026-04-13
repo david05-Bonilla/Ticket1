0df7323c8af39bd6153c8274a43d1e6b52e6d937
david05-Bonilla <bonilla.coca2027@hilasal.superate.org.sv>
2026-04-13 17:35:06 -0600

    second

--- js/logic.js
+++ js/logic.js
@@ -3,15 +3,18 @@ function shippingCost() {
     let isPrimiun = document.getElementById("isPremium").value === "true";
     let shipping = (isPrimiun || orderCost > 20) ? document.getElementById("ShippingCost").textContent = "The shipping cost is free" : document.getElementById("ShippingCost").textContent = "The shipping cost is 2 dollars"
 
+
     try {
         if (orderCost < 0) {
             throw new Error("Negative value");
         }
     } catch (e) {
+        orderCost = Math.abs(orderCost);
+        
+        document.getElementById("totalOrder").value = orderCost;
+
         alert("Ingrese un valor verdadero");
     }
 
-    if(orderCost < 0){
-        ShippingCost.textContent = "Not valid"
-    }
+    
 }
